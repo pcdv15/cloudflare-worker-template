d32886c826c944acecfcd776c0f67630db50c598
Paul Chester Villanueva <paulvillanueva2012@gmail.com>
2026-04-29 11:55:47 +0800

    feat(15): add cors handling

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,13 @@ import { createSwaggerDocument } from "@/swagger/document";
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { swaggerUI } from "@hono/swagger-ui";
 import { logger } from "hono/logger";
+import { cors } from "hono/cors";
 
 const app = new OpenAPIHono<{ Bindings: Cloudflare.Env }>();
 
 app.use("/api/*", logger());
+
+app.use("/api/*", cors());
 
 app.doc("/doc", createSwaggerDocument());
 
